de7b054bcce74eabfb693223919caf0707193898
Axel <57622898+AxelElg@users.noreply.github.com>
2023-05-11 14:09:42 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import { createUseStyles, useTheme } from 'react-jss';
+import React, { useState } from "react";
+import { useTheme } from "react-jss";
 
-import Container from './components/Container';
-import Counter from './components/Counter';
-import RepoSegment from './components/RepoSegment';
+import Container from "./components/Container";
+import Counter from "./components/Counter";
+import RepoSegment from "./components/RepoSegment";
 
-import useGlobalStyles from './styles/globalStyles';
+import useGlobalStyles from "./styles/globalStyles";
 
 export default function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	const theme = useTheme();
-	useGlobalStyles({ theme });
+  const theme = useTheme();
+  useGlobalStyles({ theme });
 
-	return (
-		<Container>
-			<Counter count={count} setCount={setCount} />
-			<RepoSegment count={count} />
-		</Container>
-	);
+  return (
+    <Container>
+      <Counter count={count} setCount={setCount} />
+      <RepoSegment count={count} />
+    </Container>
+  );
 }
